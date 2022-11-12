6ead16ad1d3dea53f5b952d9f6291eed0d981ebe
Daniel Muxel <daniel.muxel@gmail.com>
2022-11-12 10:58:35 +0100

    added website stuffz

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
 
-})
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL,
+    BACKEND_URL: process.env.BACKEND_URL,
+  },
+});
---
+++ frontend/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div class="">
+      <h1>index page</h1>
+    </div>
+    <!-- <BlocksRenderer :blocks="data?.data?.attributes?.blocks" /> -->
+  </div>
+</template>
